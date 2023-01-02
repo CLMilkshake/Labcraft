1a70e45ac7745655a738305abfc3f69e83a778de
CLMilkshake <88739857+CLMilkshake@users.noreply.github.com>
2023-01-01 20:17:12 -0700

    Working on creating accounts

--- login.js
+++ login.js
@@ -40,3 +40,27 @@ function getInfo() {
 	var element = document.getElementById("incorrectPassword");
 	element.style.opacity = "1";
 }
+
+function createAccount()
+{
+	var username = document.getElementById('createUsername').value
+	var password = document.getElementById('createPassword').value
+	
+	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches username and password of a current index of the objPeople array
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			console.log(username + " is logged in!!!")
+			
+            document.getElementById("Username").innerHTML = user;
+			var signedIn = true
+			localStorage.setItem("user", username);
+			// stop the function if this is found to be true
+			window.location = "https://clmilkshake.github.io/Labcraft/index.html"
+			return
+		}
+	}
+
+	console.log("incorrect username or password")
+	var element = document.getElementById("incorrectPassword");
+	element.style.opacity = "1";
+}
